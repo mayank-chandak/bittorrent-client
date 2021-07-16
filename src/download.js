058c1da3d8537e606ae30136b8756b68a15ec4a9
mayank-chandak <53708861+mayank-chandak@users.noreply.github.com>
2021-07-17 00:17:19 +0530

    Added handshake handler

--- src/download.js
+++ src/download.js
@@ -3,22 +3,21 @@
 const net = require('net');
 const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
+const message = require('./message.js');
 
 module.exports = torrent => {
     tracker.getPeers(torrent, peers => {
-        peers.forEach(download);
+        peers.forEach(download(peer, torrent));
     });
 };
 
-function download(peer) {
-    const socket = net.Socket();
+function download(peer, torrent) {
+    const socket = new net.Socket();
     socket.on('error', console.log);
     socket.connect(peer.port, peer.ip, () => {
-
+        socket.write(message.buildHandShake(torrent));
     });
-    onWholeMsg(socket, data => {
-
-    });
+    onWholeMsg(socket, msg => msgHandler(msg, socket));
 }
 
 function onWholeMsg(socket, callback) {
@@ -37,4 +36,13 @@ function onWholeMsg(socket, callback) {
             handshake = false;
         }
     });
+}
+
+function msgHandler(msg, socket) {
+    if (isHandshake(msg)) socket.write(message.buildInterested());
+}
+
+function isHandshake(msg) {
+    return msg.length === msg.readUInt8(0) + 49 &&
+        msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
