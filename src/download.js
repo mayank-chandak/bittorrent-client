d2930371f7e3b15528f3d7437a0b9030ca35f366
mayank-chandak <53708861+mayank-chandak@users.noreply.github.com>
2021-07-17 00:08:22 +0530

    Modified download function in download.js and added onWholeMsg to ease combining parts of a broken message

--- src/download.js
+++ src/download.js
@@ -16,7 +16,25 @@ function download(peer) {
     socket.connect(peer.port, peer.ip, () => {
 
     });
-    socket.on('data', data => {
+    onWholeMsg(socket, data => {
 
+    });
+}
+
+function onWholeMsg(socket, callback) {
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuf => {
+        const msgLen = () => handshake ? 
+            savedBuf.readUInt8(0) + 49 : 
+            savedBuf.readInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+        while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+            callback(savedBuf.slice(0, msgLen()));
+            savedBuf = savedBuf.slice(msgLen());
+            handshake = false;
+        }
     });
 }
