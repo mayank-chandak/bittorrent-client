67f7b569f0f42ee35ba1bf73366da670aef0a81f
mayank-chandak <53708861+mayank-chandak@users.noreply.github.com>
2021-07-16 23:19:26 +0530

    Declared functions in message.js

---
+++ src/message.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Buffer = require('buffer').Buffer;
+const torrentParser = require('./torrent-parser');
+
+module.exports.buildHandShake = torrent => {
+
+};
+
+module.exports.buildKeepAlive = () => {
+    
+};
+
+module.exports.buildChoke = () => {
+
+};
+
+module.exports.buildUnchoke = () => {
+
+};
+
+module.exports.buildInterested = () => {
+
+};
+
+module.exports.buildUninterested = () => {
+
+};
+
+module.exports.buildHave = payload => {
+
+};
+
+module.exports.buildBitfield = bitfield => {
+
+};
+
+module.exports.buildRequest = payload => {
+
+};
+
+module.exports.buildPiece = payload => {
+
+};
+
+module.exports.buildCancel = payload => {
+
+};
+
+module.exports.buildPort = payload => {
+
+};
